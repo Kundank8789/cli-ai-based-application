dd53c126f6eb25640ca51423bded77d7ad526717
kundan <kundan8789@gmail.com>
2025-11-21 23:44:13 +0530

    create sign-in page

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { toNodeHandler } from 'better-auth/node';
+import cors from 'cors';
+import { auth } from './lib/auth.js';
 dotenv.config();
 
 const app = express();
+
+app.use(
+    cors({
+        origin: 'http://localhost:3001', 
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    })
+)
+
+app.all('/api/auth/*splat', toNodeHandler(auth));
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+
+app.get("/api/me", async (req, res) => {
+    const session = await auth.api.getSession({
+        headers: req.headers
+    })
+    return res.json(session);
+});
+
 app.get('/', (req, res) => {
     res.send('Hello from the server!');
 });
